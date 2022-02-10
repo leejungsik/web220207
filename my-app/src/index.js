ffa4867cb997878a413c1505da1156ac5b509e3a
Leejungsik <godsider@naver.com>
2022-02-10 11:38:06 +0900

    3일차실습

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,41 +1,40 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Calc extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: 0
-        };
-        this.add = this.add.bind(this);
-        this.minus=this.minus.bind(this);
-    }
+const user = {
+    firstName :'jina',
+    lastName: 'kim'
+}
+function today() {
+    let year = new Date().getFullYear();
+    let month = new Date().getMonth() + 1;
+    let day = new Date().getDate();
 
-    add() {
-        this.setState(state => ({
-            value: state.value + 1
-        }));
-    }
+    const today = <h4>Today: {year} / {month} / {day}</h4>
+    return today
 
-    minus() {
-        this.setState(state => ({
-            value: state.value - 1
-        }));
+}
+function SayHello() {
+    let time = new Date().getHours();
+    if(time >= 17 || time < 6) {
+        return <h1>Good Night!</h1>
+    }else {
+        return <h1>Good Morning</h1>
     }
-
+}
+class HelloWorld extends React.Component {
     render() {
-        return (
-            <div>
-                <p>현재 값 : {this.state.value}</p>
-                <button onClick={this.add}>+</button>
-                <button onClick={this.minus}>-</button>
-            </div>
-        )
+        return <div>
+                    <h1>Hello, {user.firstName}{user.lastName}!</h1>
+                    {today()}
+                    <h3>{SayHello()}</h3>
+                </div>;
     }
 }
 
 ReactDOM.render(
-    <Calc/>,
+    <HelloWorld/>,
     document.getElementById('root')
 );
