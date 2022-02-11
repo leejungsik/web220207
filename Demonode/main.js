8adb531d94db4feb0061ce5527beddd24f9f9af5
Leejungsik <godsider@naver.com>
2022-02-11 16:18:48 +0900

    금요일

--- Demonode/main.js
+++ Demonode/main.js
@@ -1,8 +1,9 @@
-var fs = require("fs");
+var http = require("http"); 
 
-fs.readFile('input.txt', function (err, data) {
-    if (err) return console.error(err);
-    console.log(data.toString());
-});
+http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type':'text/plain'}); 
+    response.end("Hello world\n"); 
+}).listen(8081); 
 
-console.log("Program has ended");
+console.log("Server running http://127.0.0.1:8081"); 
+
---
+++ demo.py
@@ -0,0 +1,6 @@
+value = 5
+while value >0:
+    print(value)
+    value -= 1
+
+print("Done")
